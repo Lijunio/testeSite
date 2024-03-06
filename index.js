e59255e59e3b56f6efbba7fe0d33614276483ee8
Lijunio <112104399+Lijunio@users.noreply.github.com>
2024-03-06 14:39:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-const express = require("express");
-const jwt = require("jsonwebtoken");
+// you will need to install via 'npm install jsonwebtoken' or in your package.json
 
-const app = express();
+var jwt = require("jsonwebtoken");
 
-const METABASE_SITE_URL = "https://adm.telessaude.hc.ufmg.br";
-const METABASE_SECRET_KEY = "2a8605056ee1d091911b4c3a21a9df91081092e53889704787584ce649596e2d";
+var METABASE_SITE_URL = "https://adm.telessaude.hc.ufmg.br";
+var METABASE_SECRET_KEY = "2a8605056ee1d091911b4c3a21a9df91081092e53889704787584ce649596e2d";
 
-app.get("/", (req, res) => {
-  const payload = {
-    resource: { dashboard: 144 },
-    params: {},
-    exp: Math.round(Date.now() / 1000) + (10 * 60) // 10-minute expiration
-  };
+var payload = {
+  resource: { dashboard: 144 },
+  params: {},
+  exp: Math.round(Date.now() / 1000) + (10 * 60) // 10 minute expiration
+};
+var token = jwt.sign(payload, METABASE_SECRET_KEY);
 
-  const token = jwt.sign(payload, METABASE_SECRET_KEY);
-
-  console.log("Token gerado:", token); // Adicione esta linha para exibir o token no console do servidor Node.js
-
-  res.send(token);
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+var iframeUrl = METABASE_SITE_URL + "/embed/dashboard/" + token + "#theme=night&bordered=true&titled=true";
